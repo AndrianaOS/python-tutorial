8e829df0446d9e9d398f6bd6fbf740ba7550a19d
Andriana <andrianasaffo@gmail.com>
2023-10-05 15:39:11 +0100

    Update string-index-slice.py
    String indexing and slicing

--- Data-basics/string-index-slice.py
+++ Data-basics/string-index-slice.py
@@ -12,9 +12,32 @@ print("----------------")
 
 #Slice - Grabbing a subsection
 #Syntax: [start:stop:step]
+#step decides how many jumps to make
 
 print(my_sentence[4:9])
 
 print("----------------")
 
 print(len(my_sentence))
+
+print("----------------")
+
+print(my_sentence[::]) #Console entire sentence from start to end
+
+print("----------------")
+
+print(my_sentence[::2]) #Console from start to end but in jumps of 2 step sizes
+
+my_alphabet = "abcdeffghijklmnopqrstuvwxyz"
+
+print("----------------")
+
+print(my_alphabet[::2])
+
+print("----------------")
+
+print(my_alphabet[::3])
+
+print("----------------")
+
+print(my_alphabet[1:10:3])
