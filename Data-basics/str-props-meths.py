7ba02bb0f7f480e7b7342ca7d7aa7519ef3cffbd
Andriana <andrianasaffo@gmail.com>
2023-10-26 10:46:14 +0100

    Create file to work on string properties and methods

---
+++ Data-basics/str-props-meths.py
@@ -0,0 +1,21 @@
+name =  "Sam"
+print(name)
+
+#Change name to Pam
+
+last_letters = name[1:]
+name = "P" + last_letters
+
+print(name)
+
+
+#Using multiplication on strings increases the amount of strings by the number 
+word = "hello"
+print(word * 3)
+
+
+phrase = "hello World"
+print(phrase.capitalize())
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.split())
